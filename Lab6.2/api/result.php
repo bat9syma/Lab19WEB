e52febc9b31da7d3f99d7caf75b1bd2a9ab48ca9
bat9syma <133958260+bat9syma@users.noreply.github.com>
2025-05-21 00:31:22 +0300

    Update result.php

--- Lab6.2/api/result.php
+++ Lab6.2/api/result.php
@@ -16,19 +16,19 @@ $result = calculateExpression($x, $y, $z);
 <html lang="uk">
 <head>
   <meta charset="UTF-8">
-  <title>Результат</title>
+  <title>Result</title>
 </head>
 <body>
-  <h2>Результат обчислення</h2>
-  <p><strong>ПІБ:</strong> <?php echo htmlspecialchars("$lastname $firstname"); ?></p>
-  <p><strong>Група:</strong> <?php echo htmlspecialchars($group); ?></p>
-  <p><strong>Варіант:</strong> <?php echo htmlspecialchars($variant); ?></p>
+  <h2>Calculation Result</h2>
+  <p><strong>Full name:</strong> <?php echo htmlspecialchars("$lastname $firstname"); ?></p>
+  <p><strong>Group:</strong> <?php echo htmlspecialchars($group); ?></p>
+  <p><strong>Variant:</strong> <?php echo htmlspecialchars($variant); ?></p>
   <hr>
   <p><strong>X:</strong> <?php echo round($x, 2); ?></p>
   <p><strong>Y:</strong> <?php echo round($y, 2); ?></p>
   <p><strong>Z:</strong> <?php echo round($z, 2); ?></p>
   <hr>
   <p><strong>f(x, y, z):</strong> <?php echo round($result, 4); ?></p>
-  <br><a href="/index.html">← Повернутись назад</a>
+  <br><a href="/index.html">← Go Back</a>
 </body>
 </html>
